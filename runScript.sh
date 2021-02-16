c2aad14a1fc93c899e6532abfe2eb64e5424c961
Hain Zuppur <zuppur@rocket.hpc.ut.ee>
2021-02-16 15:03:48 +0200

    changed metadata gen

--- runScript.sh
+++ runScript.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:tesla:1
 
 #The name of the job is test_job
-#SBATCH -J test_job
+#SBATCH -J cotatron
 
 #The job requires 1 compute node
 #SBATCH -N 1
@@ -12,8 +12,7 @@
 #The job requires 1 task per node
 #SBATCH --ntasks-per-node=1
 
-#The maximum walltime of the job is a half hour
-#SBATCH -t 00:30:00
+#SBATCH -t 01:00:00
 
 #These commands are run on one of the nodes allocated to the job (batch node)
 
@@ -23,4 +22,4 @@ source /storage/software/python-3.6.3/miniconda3/etc/profile.d/conda.sh
 
 conda activate cotatron_env
 
-python cotatron_trainer.py -c config/global/default.yaml config/cota/default.yaml -g 0 -n my_runname
+python cotatron_trainer.py -c config/global/default.yaml config/cota/default.yaml -g 0 -n cotatron_trained -f True
