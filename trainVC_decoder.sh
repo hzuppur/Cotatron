0c55fc99a7d22debc4e6ea514874c3d44b943864
Hain Zuppur <zuppur@rocket.hpc.ut.ee>
2021-03-31 00:37:41 +0300

    trained vc for 5 days

--- trainVC_decoder.sh
+++ trainVC_decoder.sh
@@ -12,7 +12,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=32G
 
-#SBATCH -t 48:00:00
+#SBATCH -t 192:00:00
 
 #These commands are run on one of the nodes allocated to the job (batch node)
 
@@ -22,4 +22,4 @@ source /storage/software/python-3.6.3/miniconda3/etc/profile.d/conda.sh
 
 conda activate cotatron_env
 
-python synthesizer_trainer.py -c config/global/default.yaml config/vc/default.yaml -g 0 -n cotatron_trained_vc
+python synthesizer_trainer.py -c config/global/default.yaml config/vc/default.yaml -g 0 -n cotatron_trained_vc -p chkpt/vc/cotatron_trained_vc/3ba23aeepoch=25.ckpt
